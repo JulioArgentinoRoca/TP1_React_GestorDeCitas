a4655a8fdbd6d2614c7ceb57ec1d850176323aae
Android Studio <devandroid@ort>
2026-03-25 08:49:01 -0300

    finished!

--- src/App.jsx
+++ src/App.jsx
@@ -9,29 +9,41 @@ import './App.css'
 function App() {
   
   return (
-    <div id="root">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"/>
-    <link href="https://fonts.googleapis.com/css?family=Staatliches" rel="stylesheet"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css"/>
-      <h1>ADMINISTRADOR DE PACIENTES</h1>
-      <div class="container">
-        <div class="row">
-          <div class="one-half column">
-            < Formulario />
+    <html>
+      <head>
+        <link rel="apple-touch-icon" href="/logo192.png"/>
+        <link rel="manifest" href="/manifest.json"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Staatliches" rel="stylesheet"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css"/>
+      </head>
+      
+      <body>
+        <div id="root">
+        
+          <h1>ADMINISTRADOR DE PACIENTES</h1>
+          <div class="container">
+            <div class="row">
+              <div class="one-half column">
+                < Formulario />
+              </div>
+              
+              <div class="one-half column">
+                < Lista />
+              </div>
+              
+            </div>
           </div>
-          
-          <div class="one-half column">
-            < Lista />
-          </div>
+
+          <script src="/static/js/bundle.js"></script>
+          <script src="/static/js/vendors~main.chunk.js"></script>
+          <script src="/static/js/main.chunk.js"></script>
           
         </div>
-      </div>
-
-      <script src="/static/js/bundle.js"></script>
-      <script src="/static/js/vendors~main.chunk.js"></script>
-      <script src="/static/js/main.chunk.js"></script>
+      </body>
       
-    </div>
+    </html>
+    
   )
 }
 
