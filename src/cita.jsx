34070e52d903ca34b73992321cf5ce9432bb0bd5
LUCA GAMAS DE MAIO <49008593@ort>
2026-04-14 14:02:07 -0300

    added a confirm() to delete appointment

--- src/cita.jsx
+++ src/cita.jsx
@@ -2,13 +2,20 @@ import './cita.css'
 
 function appointment(props){
 
+    const deleteAppointment = () =>{
+        
+        if(window.confirm("Desea eliminar esta cita?")){
+            props.deletMev2(props.myIndex)
+        }
+    }
+
     return (
         <div className="cita">
               <p>Mascota: <span>{props.pet}</span></p>
               <p>Dueño: <span>{props.owner}</span></p>
               <p>Fecha: <span>{props.date}</span></p>
               <p>Hora: <span>{props.time}</span></p>
-              <p>Sintomas: <span>{props.syntoms}</span></p><button className="button elimnar u-full-width" onClick={() => props.deletMev2(props.myIndex)}>Eliminar ×</button>
+              <p>Sintomas: <span>{props.syntoms}</span></p><button className="button elimnar u-full-width" onClick={() => deleteAppointment()}>Eliminar ×</button>
         </div>
     )
 }
